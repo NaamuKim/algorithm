6c6f23a8a3f9b21e729ff089038d0172c006b622
superwonso <chadonghyeon@kakao.com>
2021-07-25 03:27:18 +0900

    20210725
    tq

--- chahyeoncha/2nd week/p2/study_w2_p2.go
+++ chahyeoncha/2nd week/p2/study_w2_p2.go
@@ -1,28 +1,44 @@
-package main
+package main //proof to bf
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
-func PrintCombination() {
-	var n, m int
-	var r, k int
-	fmt.Scanln(&n, &m)
-	fmt.Scanln(&r, &k)
-	fmt.Println(Combination(r, k))
-}
-
-func factorial(n int) int {
-	if n <= 0 {
-		return 1
+func main() {
+	var n, m, result int
+	fmt.Scanf("%d %d", &n, &m)
+	var cases [9][19]int
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			r := bufio.NewReader(os.Stdin)
+			fmt.Fscan(r, &cases[i][j])
+		}
 	}
-	return (n * factorial(n-1))
-}
-
-func Permutation(n, k int) int {
-	return factorial(n) / factorial(n-k)
-}
-
-func Combination(n, k int) int {
-	return Permutation(n, k) / factorial(k)
+	for x := 0; x <= n; x++ {
+		for y := 0; y <= n; y++ {
+			flag := 0
+			for w := 0; w < m; w++ {
+				result_x := 0
+				result_y := 0
+				for z := 0; z < n; z++ {
+					if cases[w][z] == x {
+						result_x = z
+					}
+					if cases[w][z] == y {
+						result_y = z
+					}
+				}
+				if result_x < result_y {
+					flag++
+				}
+			}
+			if flag == m {
+				// fmt.Println(cases[x][y])
+				result++
+			}
+		}
+	}
+	fmt.Println(result - 1)
 }
