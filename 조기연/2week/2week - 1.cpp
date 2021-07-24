2bb3900940ed05b0772fd2db45becce5588fd8b9
unknown <hsky8520@cbnu.ac.kr>
2021-07-24 22:59:38 +0900

    2주차 문제

---
+++ 조기연/2week/2week - 1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+int digit_sum(int n)
+{
+    int sum = 0;
+    while (n != 0)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+
+    return sum;
+}
+
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int max_num = 0, max_sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+
+        int sum = digit_sum(num);
+        if (max_sum < sum)
+        {
+            max_num = num;
+            max_sum = sum;
+        }
+        else if (max_sum == sum && max_num < num)
+        {
+            max_num = num;
+        }
+    }
+
+    cout << max_num << endl;
+
+    return 0;
+}
