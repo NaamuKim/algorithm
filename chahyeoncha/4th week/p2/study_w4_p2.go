350fe26005778eadb88bf528fa2001987369c8d8
superwonso <chadonghyeon@kakao.com>
2021-08-21 15:27:00 +0900

    20210821

--- chahyeoncha/4th week/p2/study_w4_p2.go
+++ chahyeoncha/4th week/p2/study_w4_p2.go
@@ -1,26 +1,25 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 )
 
 func main() {
-	var N int
-	temp := bufio.NewReader(os.Stdin)
-	fmt.Fscan(temp, &N)
-
+	array := [5][5]int{
+		{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1},
+	}
+	// temp := bufio.NewReader(os.Stdin)
+	const N int = 5
+	/* fmt.Fscan(temp, &N)
 	array := make([][]int, N)
-	for a := range array {
+	 for a := range array {
 		array[a] = make([]int, N)
 	}
 	for j := 0; j < N; j++ {
 		for i := 0; i < N; i++ {
 			fmt.Scanf("%d", &array[i][j])
 		}
-	} // empty slice(=array) is filled to "0"
-
+	} // empty slice(=array) is filled to "0"*/
 	move_list := make([]int, N)
 	for i := 0; i < N; i++ {
 		fmt.Scanf("%d", &move_list[i])
@@ -29,10 +28,10 @@ func main() {
 	move(move_list, array, basket, N)
 	answer := 0
 	clear(basket, N, answer)
-	fmt.Println(answer)
+	fmt.Println(2 * answer)
 }
 
-func move(move []int, from [][]int, target []int, N int) {
+func move(move []int, from [5][5]int, target []int, N int) {
 	for i := 0; i < N; i++ {
 		if move[i] == 0 {
 			i = i + 1
@@ -55,6 +54,9 @@ func clear(basket []int, N int, answer int) {
 		if basket[i-1] == basket[i] {
 			for j := i; j < N; j++ {
 				basket[j-1] = basket[j+1]
+				basket[j] = basket[j+2]
+				basket[j+1] = 0
+				basket[j+2] = 0
 				answer++
 			}
 
