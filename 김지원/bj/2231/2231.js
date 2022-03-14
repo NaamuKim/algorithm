b14d478773fa3018c5de33220f9348891b34a5b9
Jiwon Kim <lost731@naver.com>
2022-03-14 16:14:06 +0900

    Add: bj 10926

--- 김지원/bj/2231/2231.js
+++ 김지원/bj/2231/2231.js
@@ -1,20 +1,29 @@
-const fs=require("fs");
-const filePath=process.platform==='linux' ? "/dev/stdin" : './input.txt';
-let input= fs.readFileSync(filePath).toString().trim().split('\n');
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-let arr=[];
+let arr = [];
 solution(+input[0]);
 
-function splitSum(n){
-    let num=n.toString().split("").map(item=>+item).reduce((a,b)=>a+b);
-    return num+n;
+function splitSum(n) {
+  let num = n
+    .toString()
+    .split('')
+    .map((item) => +item)
+    .reduce((a, b) => a + b);
+  return num + n;
 }
 
 function solution(n) {
-    for(let i=0;i<=n;i++){
-        if(splitSum(i)===n) arr.push(i);
-    }
-    arr.sort((a,b)=>a-b);
-    if(arr[0])console.log(arr[0]);
-    else console.log(0);
+  for (let i = 0; i <= n; i++) {
+    if (splitSum(i) === n) arr.push(i);
+  }
+  arr.sort((a, b) => a - b);
+  if (arr[0]) console.log(arr[0]);
+  else console.log(0);
 }
+[
+  [1, 1],
+  [2, 2],
+  [4, 4],
+];
