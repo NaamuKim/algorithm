2aad59ce3b80707c6be5cbde81f450341dd9214d
Jiwon Kim <lost731@naver.com>
2022-03-26 10:26:48 +0900

    Add: 송아지 찾기(BFS)

--- 김지원/bj/app.js
+++ 김지원/bj/app.js
@@ -11,26 +11,11 @@ const check = Array.from({ length: xlength }, () => Array(ylength).fill(0));
 console.log(solution(arr));
 
 function solution(board) {
-  const dx = [-1, 0, 1, 0];
-  const dy = [0, 1, 0, -1];
-
-  function BFS(x, y) {
-    const queue = [];
-    queue.push([x, y]);
-
-    check[x][y] = 1;
-    while (queue.length) {
-      const [x, y] = queue.shift();
-      for (let i = 0; i < 4; i++) {
-        const [nx, ny] = [x + dx[i], y + dy[i]];
-        if (nx < 0 || ny < 0 || nx >= xlength || ny >= ylength) continue;
-        if (board[x][y] === 1 && !check[nx][ny]) {
-          check[nx][ny] = check[x][y] + 1;
-          queue.push([nx, ny]);
-        }
-      }
-    }
+  let answer = 0;
+  function DFS(x, y) {
+    const dx = [-1, 0, 1, 0];
+    const dy = [0, 1, 0, -1];
   }
-  BFS(0, 0);
-  return check[xlength - 1][ylength - 1];
+  DFS(0, 0);
+  return;
 }
