e050c0034d6321bfaa70c1ef9a10d63b6734efc7
Jiwon Kim <lost731@naver.com>
2022-04-05 15:34:05 +0900

    Add: cpp 알고리즘 연습

--- 김지원/newAlgo/e.cpp
+++ 김지원/newAlgo/e.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int main()
 {
     vector<int> v[10];
-    vector<int> v(5,0);
+
 
     cout << v << "\n";
     return 0;
     // vector<vector<int>> checked;
     // vector<vector<int>> v(n + 1, vector<int> (n + 1, 0));
     // fill(v.begin(), v.end(),0);
-
+    
     // cout << v << " ";
     // return 0;
 }
---
+++ 김지원/newAlgo/queue.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+int main(){
+    queue<int> q;
+    q.push(1);
+    cout << q.front() << "\n";
+    q.pop();
+    cout << q.size() <<  "\n";
+
+    return 0;
+}
