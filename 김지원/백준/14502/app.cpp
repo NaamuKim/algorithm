5b095c7e51018d95e6fad0ea9c340409dd260c35
Jiwon Kim <lost731@naver.com>
2022-04-18 13:44:34 +0900

    Add: 백준 14502

--- 김지원/백준/14502/app.cpp
+++ 김지원/백준/14502/app.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
-typedef pair<int,int> pi;
 using namespace std;
-vector<pi> vc;
+
+typedef pair<int,int> pi;
+vector<pi> v;
 int n, m;
 int a[10][10];
 bool vis[10][10];
@@ -9,13 +10,30 @@ int dy[4]={0, 1, 0, -1};
 int dx[4]={1, 0, -1, 0};
 
 void dfs(int y, int x){
+    if(a[y][x] == 1 || vis[y][x]) return;
     vis[y][x]=true;
     for(int i =0; i<4; i++){
         int ny = y+dy[i];
-        int nx = x+dy[i];
-        if(ny <0 || nx <0 || ny>n-1 || nx >m-1|| vis[ny][nx]) continue;
+        int nx = x+dx[i];
+        if(ny <0 || nx <0 || ny>n-1 || nx >m-1) continue;
         dfs(ny, nx);
     }
+}
+
+int solve(){
+    memset(vis, 0, sizeof(vis));
+    for(int i =0; i<n; i++){
+        for(int j =0; j<m; j++){
+            if(a[i][j]==2) dfs(i,j);
+        }
+    }
+    int ret =0;
+    for(int i= 0; i<n;i++){
+        for(int j =0; j<m; j++){
+            if(a[i][j]==0&&!vis[i][j]) ret++;
+        }
+    }
+    return ret;
 }
 
 int main(){
@@ -23,8 +41,19 @@ int main(){
     for(int i=0; i<n;i++){
         for(int j=0; j<m;j++){
             cin >> a[i][j];
-            if(!a[i][j]) vc.push_back(pi(i,j));
+            if(!a[i][j]) v.push_back(pi(i,j));
         }
     }
-    int res=0;
+    int ret = 0;
+    for(int i =0; i < v.size(); i++){
+        for(int j =0; j < i; j++){
+            for(int k =0; k < j; k++){
+                a[v[i].first][v[i].second]=a[v[j].first][v[j].second]=a[v[k].first][v[k].second] =1;
+                ret=max(ret,solve());
+                a[v[i].first][v[i].second]=a[v[j].first][v[j].second]=a[v[k].first][v[k].second] =0;    
+            }
+        }
+    }
+    cout << ret;
+    return 0;
 }
