c48ff681019ec9b98530ab8a32f240a94f986bab
Jiwon Kim <lost731@naver.com>
2022-04-18 19:25:57 +0900

    Add: review 백준 2828

---
+++ 김지원/review/2828/app.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m, k, j, ret;
+
+int main(){
+    cin >> n >> m >> j;
+    int l =1;
+    for(int i =0; i<j; i++){
+        int r=l+m-1;
+        cin >> k;
+        if(k>=l && k<=r) continue;
+        else{
+            if(k<l){
+                ret+=(l-k);
+                l=k;
+            } else{
+                l+=(k-r);
+                ret+=(k-r);
+            }
+        }
+    }
+    cout << ret <<"\n";
+    return 0;
+}
