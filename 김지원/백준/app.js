75d4b9674d56cda265ca55c23a0ed18e80e70338
Jiwon Kim <lost731@naver.com>
2023-01-11 21:22:22 +0900

    Add: 백준 7569 토마토

--- 김지원/백준/app.js
+++ 김지원/백준/app.js
@@ -1,50 +1,62 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
-const n = +input.shift();
-const dy = [0, 1, 0, -1];
-const dx = [1, 0, -1, 0];
-let arr = input.map((item) => item.split(''));
-let vis = Array.from({ length: n }, () => Array.from({ length: n }).fill(0));
+
+const [m, n, h] = input.shift().split(' ').map(Number);
+const queue = [];
+const dy = [0, 1, 0, -1, 0, 0];
+const dx = [1, 0, -1, 0, 0, 0];
+const dz = [0, 0, 0, 0, 1, -1];
+let arr = [];
+for (let i = 0; i < input.length; i += n) {
+  arr[Math.floor(i / n)] = input.slice(i, i + n).map((item) => item.split(' ').map(Number));
+}
+let day = 0;
+let vis = Array.from({ length: h }, () => Array.from({ length: n }, () => Array.from({ length: m }).fill(0)));
 
 const solution = () => {
-  let cnt1 = 0,
-    cnt2 = 0;
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < h; i++) {
     for (let j = 0; j < n; j++) {
-      if (!vis[i][j]) {
-        dfs(i, j, arr[i][j]);
-        cnt1++;
+      for (let k = 0; k < m; k++) {
+        if (arr[i][j][k] === 1) {
+          queue.push([i, j, k]);
+        }
       }
     }
   }
-  vis = Array.from({ length: n }, () => Array.from({ length: n }).fill(0));
-  arr = arr.map((item) => item.map((each) => (each === 'R' ? 'G' : each)));
 
-  for (let i = 0; i < n; i++) {
+  bfs();
+  for (let i = 0; i < h; i++) {
     for (let j = 0; j < n; j++) {
-      if (!vis[i][j]) {
-        dfs(i, j, arr[i][j]);
-        cnt2++;
+      for (let k = 0; k < m; k++) {
+        if (arr[i][j][k] === 0) return -1;
+        day = Math.max(vis[i][j][k], day);
       }
     }
   }
-  return cnt1 + ' ' + cnt2;
+  return day;
 };
 
-const dfs = (y, x, item) => {
-  for (let k = 0; k < dy.length; k++) {
-    const ny = y + dy[k];
-    const nx = x + dx[k];
-    if (isOnRange(ny, nx) && arr[ny][nx] === item) {
-      vis[ny][nx] = 1;
-      dfs(ny, nx, arr[ny][nx]);
+const bfs = () => {
+  while (queue.length) {
+    const [z, y, x] = queue.shift();
+    for (let k = 0; k < 6; k++) {
+      const ny = y + dy[k];
+      const nx = x + dx[k];
+      const nz = z + dz[k];
+      if (!isOnRange(ny, nx, nz) || arr[nz][ny][nx] === 1 || arr[nz][ny][nx] === -1) continue;
+      arr[nz][ny][nx] = 1;
+      if (vis[nz][ny][nx] === 0) vis[nz][ny][nx] = vis[z][y][x] + 1;
+      else {
+        vis[nz][ny][nx] = Math.min(vis[nz][ny][nx], vis[z][y][x] + 1);
+      }
+      queue.push([nz, ny, nx]);
     }
   }
 };
 
-const isOnRange = (i, j) => {
-  if (i < 0 || i >= n || j < 0 || j >= n || vis[i][j] === 1) return false;
+const isOnRange = (y, x, z) => {
+  if (y < 0 || y >= n || x < 0 || x >= m || z < 0 || z >= h) return false;
   return true;
 };
 
