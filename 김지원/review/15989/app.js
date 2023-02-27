eab99aa719b18b98d46a1a89e601748a5e7500d5
Jiwon Kim <lost731@naver.com>
2023-02-27 23:05:29 +0900

    Add: 백준 19989 (dp) 1, 2, 3 더하기 4 review

---
+++ 김지원/review/15989/app.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n').map(Number);
+
+let t = +input.shift();
+
+const solution = () => {
+  let i = 0;
+  const dp = Array.from({ length: 10001 }).fill(0);
+  dp[0] = 1;
+  for (let i = 1; i <= 3; i++) {
+    for (let j = 1; j <= 10001; j++) {
+      if (j >= i) dp[j] += dp[j - i];
+    }
+  }
+  let ret = '';
+  while (t--) {
+    if (!input[i]) return ret;
+    ret += dp[input[i]] + '\n';
+    i++;
+  }
+  return ret;
+};
+
+console.log(solution());
