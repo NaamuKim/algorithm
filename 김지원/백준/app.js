670c27b47590aa766127f8a50035ca7a36fab453
Jiwon Kim <lost731@naver.com>
2023-02-27 22:43:37 +0900

    Add: 백준 2240 자두 (DP) review

--- 김지원/백준/app.js
+++ 김지원/백준/app.js
@@ -1,25 +1,25 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = fs.readFileSync(filePath).toString().trim().split('\n');
+const input = fs.readFileSync(filePath).toString().trim().split('\n').map(Number);
 
-const [t, w] = input.shift().split(' ').map(Number);
-// 3차원 배열 [나무][어떤 나무][이동]
-const dp = Array.from({ length: 1004 }, () => Array.from({ length: 2 }, () => Array.from({ length: 34 }).fill(-1)));
-
-const go = (idx, tree, cnt) => {
-  if (cnt < 0) return -1e9;
-  if (idx === t) return cnt == 0 ? 0 : -1e9;
-
-  const ret = dp[idx][tree][cnt];
-  if (ret !== -1) return ret;
-
-  // 움직이거나 안 움직이는 것
-  return (dp[idx][tree][cnt] =
-    Math.max(go(idx + 1, tree ^ 1, cnt - 1), go(idx + 1, tree, cnt)) + Number(tree == +input[idx] - 1));
-};
+let t = +input.shift();
 
 const solution = () => {
-  return Math.max(go(0, 1, w - 1), go(0, 0, w));
+  let i = 0;
+  const dp = Array.from({ length: 10001 }).fill(0);
+  dp[0] = 1;
+  for (let i = 1; i <= 3; i++) {
+    for (let j = 1; j <= 10000; j++) {
+      if (j - i >= 0) dp[j] += dp[j - i];
+    }
+  }
+  let ret = '';
+  while (t--) {
+    if (!input[i]) return ret;
+    ret += dp[input[i]] + '\n';
+    i++;
+  }
+  return ret;
 };
 
 console.log(solution());
