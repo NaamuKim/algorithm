f7a023c22734bb5eef1e8d889d843bf308948640
Jiwon Kim <lost731@naver.com>
2023-05-01 21:33:43 +0900

    Add: 백준 11725 트리의 부모 찾기

--- 김지원/백준/11725/app.js
+++ 김지원/백준/11725/app.js
@@ -1,32 +1,35 @@
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let input = fs.readFileSync(filePath).toString().trim().split('\n');
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
 const n = +input.shift();
-const arr = input.map((item) => item.split(' ').map(Number));
-const tree = Array.from({ length: n + 1 }, () => []);
-const queue = [];
-const vis = Array.from({ length: n + 1 }, () => 0);
-const solution = () => {
-  for (let [a, b] of arr) {
-    tree[a].push(b);
-    tree[b].push(a);
-  }
-  tree[1].forEach((item) => queue.push(item));
-  vis[1];
-  bfs();
-  vis.slice(2).forEach((item) => console.log(item));
-};
+const tree = Array.from({ length: n + 1 }, () => new Array());
+input.map((val) => {
+  const [from, to] = val.split(" ").map(Number);
+  tree[from].push(to);
+  tree[to].push(from);
+});
+const q = [];
+const vis = Array.from({ length: n + 1 }).fill(0);
 
-const bfs = () => {
-  while (queue.length) {
-    const node = queue.shift();
-    for (let next of tree[node]) {
-      if (!vis[next]) {
-        vis[next] = node;
-        queue.push(next);
-      }
+function bfs() {
+  q.push(1);
+  vis[1] = -1;
+  while (q.length) {
+    const item = q.shift();
+    for (let next of tree[item]) {
+      if (vis[next]) continue;
+      vis[next] = item;
+      q.push(next);
     }
   }
-};
+}
+function solution() {
+  let answer = "";
+  bfs();
+  for (let i = 2; i <= n; i++) {
+    answer += vis[i] + "\n";
+  }
+  console.log(answer);
+}
 
 solution();
